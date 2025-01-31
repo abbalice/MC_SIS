b7af69260d365688e1c9c9995109a8d542367908
Alice Abbate <103842179+abbalice@users.noreply.github.com>
2025-01-31 16:11:03 +0100

    Update plot_curves_offshore.py

--- plot_curves_offshore.py
+++ plot_curves_offshore.py
@@ -34,10 +34,10 @@ percentage = from_local_parser()
 matplotlib.rcParams['font.family'] = 'serif'
 matplotlib.rcParams['font.sans-serif'] = 'Times'
 
-offshore_poi = ["CT", "SR", "LK"]
-city = ["Catania", "Siracusa", "Larnaka"]
-letters = ["(a)", "(b)", "(c)", "(d)", "(e)", "(f)", "(g)", "(h)", "(i)"]
-fig, axes = plt.subplots(nrows=3, ncols=3, sharex=True, sharey=True, figsize=(21,18), constrained_layout=True)
+offshore_poi = ["CT", "SR"]#, "LK"]
+city = ["Catania", "Siracusa"]#, "Larnaka"]
+letters = ["(a)", "(b)", "(c)", "(d)", "(e)", "(f)"]#, "(g)", "(h)", "(i)"]
+fig, axes = plt.subplots(nrows=3, ncols=2, sharex=True, sharey=True, figsize=(32,45), constrained_layout=True)
 
 for i, inpdir in enumerate(offshore_poi):
   # MIH at offshore POI
@@ -49,24 +49,27 @@ for i, inpdir in enumerate(offshore_poi):
   # Coordinates of SIS POIs
   if inpdir=="CT":
     poi_off = pd.read_csv('sis_poi_catania.txt', engine='python', header=None, index_col=False).to_numpy().squeeze()
-    region_map_CT = [15, 15.17, 37.3, 37.550]
+    region_map_CT = [15, 15.17, 37.3, 37.45]#37.550]
 
   elif inpdir=="SR":
     poi_off = pd.read_csv('sis_poi_siracusa.txt', engine='python', header=None, index_col=False).to_numpy().squeeze()
-    region_map_SR = [15.15, 15.4, 37, 37.2]
-
-  elif inpdir=="LK":
-    poi_off = pd.read_csv('sis_poi_larnaka.txt', engine='python', header=None, index_col=False).to_numpy().squeeze()
-    region_map_LK = [33.55, 33.75, 34.85, 35.05]
-
+    region_map_SR = [15.15, 15.4, 37, 37.15]#37.2]
+ 
   lon_sis, lat_sis = poi_off[0], poi_off[1]
 
   letter1=letters[i]
-  letter2=letters[i+3]
-  letter3=letters[i+6]
+  letter2=letters[i+2]
+  letter3=letters[i+4]
   ax1 = axes.flat[i]
-  ax2 = axes.flat[i+3]
-  ax3 = axes.flat[i+6]
+  ax2 = axes.flat[i+2]
+  ax3 = axes.flat[i+4]
+
+  rect1 = ax1.patch
+  rect1.set_facecolor("whitesmoke")
+  rect2 = ax2.patch
+  rect2.set_facecolor("whitesmoke")
+  rect3 = ax3.patch
+  rect3.set_facecolor("whitesmoke")
 
   thresholds = [1.000e-02, 5.000e-02, 1.000e-01, 2.000e-01,
        3.000e-01, 5.000e-01, 8.000e-01, 1.000e+00,
@@ -89,110 +92,129 @@ for i, inpdir in enumerate(offshore_poi):
  #=================================================================
   # Plots
   
-  true_poe, = ax1.plot(thresholds, lambda_true_mean, color='black', linewidth=2, label='Exact mean')
-  sis_poe, = ax1.plot(thresholds, lambda_sis_mean, color='darkblue', linewidth=2, label='SIS mean (new IF)')
-  new_if, = ax1.plot(thresholds, ci_new_up, color='darkblue', linestyle='-.', linewidth=1, label='Analytical 95% c.i. (new IF)')
-  ax1.plot(thresholds, ci_new_dw, color='darkblue', linestyle='-.', linewidth=1, label='')
-  ax1.fill_between(thresholds, ci_new_dw, ci_new_up, color='tab:blue', alpha=0.2)
+  true_poe, = ax1.plot(thresholds, lambda_true_mean, color='black', linewidth=7, label='Exact mean')
+  sis_poe, = ax1.plot(thresholds, lambda_sis_mean, color='#FFD700', linewidth=7, linestyle='-.', label='SIS mean (new IF)')
+  #new_if, = ax1.plot(thresholds, ci_new_up, color='darkblue', linestyle='-.', linewidth=1, label='Analytical 95% c.i. (new IF)')
+  #ax1.plot(thresholds, ci_new_dw, color='darkblue', linestyle='-.', linewidth=1, label='')
+  ax1.fill_between(thresholds, ci_new_dw, ci_new_up, color='tab:blue', alpha=0.6)
+  new_if, = ax1.fill(np.NaN, np.NaN, 'tab:blue', alpha=0.6, label="Analytical 95% c.i. (new IF)")
 
-  old_if, = ax1.plot(thresholds, ci_old_up, color='tab:red', linestyle='-', linewidth=2, label='Analytical 95% c.i. (old IF)')
-  ax1.plot(thresholds, ci_old_dw, color='tab:red', linestyle='-', linewidth=2, label='')
+  ax1.fill_between(thresholds, ci_old_dw, ci_new_dw, color='tab:red', alpha=0.6)
+  ax1.fill_between(thresholds, ci_new_up, ci_old_up, color='tab:red', alpha=0.6)
+  old_if, = ax1.fill(np.NaN, np.NaN, 'tab:red', alpha=0.6, label="Analytical 95% c.i. (old IF)")
+
+  #old_if, = ax1.plot(thresholds, ci_old_up, color='tab:red', linestyle='-', linewidth=2, label='Analytical 95% c.i. (old IF)')
+  #ax1.plot(thresholds, ci_old_dw, color='tab:red', linestyle='-', linewidth=2, label='')
   ax1.set_ylim([1e-6, 1]) 
   ax1.set_yscale('log')
   ax1.set_xlim([0, 6])
-  ax1.text(x=1, y=3*1e-1, s="N = {}".format(percentage), color="darkblue", fontsize=22)
-  ax1.text(x=0.5, y=3*1e-1, s="{}".format(letter1), color="darkblue", fontsize=22)
-  ax1.set_title("{}".format(city[i]), fontsize=25)
-  
-  plt.xticks(fontsize=25)
+  ax1.text(x=0.2, y=3*1e-6, s="N = {}".format(percentage), color="black", fontsize=55, bbox=dict(facecolor='white', edgecolor='black', pad=10.0)) 
+  ax1.text(x=0.2, y=2*1e-1, s="{}".format(letter1), color="black", fontsize=55, bbox=dict(facecolor='white', edgecolor='grey', pad=10.0))
+  #ax1.set_title("{}".format(city[i]), fontsize=45, fontweight="bold", x=0.15, y=1.4)
+  ax1.set_title("{}".format(city[i]), fontsize=55, fontweight="bold", x=0.15, y=1.06)
+
   #--------------------------
-  ax2.text(x=0.5, y=3*1e-1, s="{}".format(letter2), color="darkblue", fontsize=22)
-  ax2.fill_between(thresholds, ci_new_dw, ci_new_up, color='tab:blue', alpha=0.2)
-  mc_new_if, = ax2.plot(thresholds, mc_ci_new_up, color='hotpink', linestyle='-', linewidth=2, label='Estimated 95% c.i. (new IF)')
-  ax2.plot(thresholds, mc_ci_new_dw, color='hotpink', linestyle='-', linewidth=2, label='')
+  ax2.text(x=0.2, y=2*1e-1, s="{}".format(letter2), color="black", fontsize=55, bbox=dict(facecolor='white', edgecolor='grey', pad=10.0))
+  ax2.fill_between(thresholds, ci_new_dw, ci_new_up, color='tab:blue', alpha=0.6)
+  mc_new_if, = ax2.plot(thresholds, mc_ci_new_up, color='hotpink', linestyle='-', linewidth=7, label='Estimated 95% c.i. (new IF)')
+  ax2.plot(thresholds, mc_ci_new_dw, color='hotpink', linestyle='-', linewidth=7, label='')
   #---------------------------
-  true_percentiles1, = ax3.plot(thresholds, p2, color="darkmagenta", linewidth=2, alpha=0.6, label='Exact p2 - p98')
-  ax3.plot(thresholds, p98, color="darkmagenta", linewidth=2, alpha=0.6, label='')
-  true_percentiles2, = ax3.plot(thresholds, p16, color="darkorange", linewidth=2, alpha=0.6, label='Exact p16 - p84')
-  ax3.plot(thresholds, p84, color="darkorange", linewidth=2, alpha=0.6, label='')
-  ax3.plot(thresholds, sis_p2, color="black", linestyle=":", linewidth=2, label='')
-  ax3.plot(thresholds, sis_p98, color="black", linestyle=":", linewidth=2, label='')
-  sis_percentiles, = ax3.plot(thresholds, sis_p16, color="black", linestyle=":", linewidth=2, label='SIS percentiles')
-  ax3.plot(thresholds, sis_p84, color="black", linestyle=":", linewidth=2, label='')
-  ax3.text(x=0.5, y=3*1e-1, s="{}".format(letter3), color="darkblue", fontsize=22)
+  true_percentiles1, = ax3.plot(thresholds, p2, color="darkmagenta", linewidth=7, alpha=0.8, label='Exact p2 - p98')
+  ax3.plot(thresholds, p98, color="darkmagenta", linewidth=7, alpha=0.8, label='')
+  true_percentiles2, = ax3.plot(thresholds, p16, color="darkorange", linewidth=7, alpha=0.8, label='Exact p16 - p84')
+  ax3.plot(thresholds, p84, color="darkorange", linewidth=7, alpha=0.8, label='')
+  ax3.plot(thresholds, sis_p2, color="black", linestyle="-.", linewidth=7, label='')
+  ax3.plot(thresholds, sis_p98, color="black", linestyle="-.", linewidth=7, label='')
+  sis_percentiles, = ax3.plot(thresholds, sis_p16, color="black", linestyle="-.", linewidth=7, label='SIS percentiles (new IF)')
+  ax3.plot(thresholds, sis_p84, color="black", linestyle="-.", linewidth=7, label='')
+  ax3.text(x=0.2, y=2*1e-1, s="{}".format(letter3), color="black", fontsize=55, bbox=dict(facecolor='white', edgecolor='grey', pad=10.0))
   
   if (i==0):
-    ax1.set_ylabel("Annual exceedance-rate", fontsize=25)
-    plt.setp(ax1.get_yticklabels(), visible=True, fontsize=25)
-    ax2.set_ylabel("Annual exceedance-rate", fontsize=25)
-    plt.setp(ax2.get_yticklabels(), visible=True, fontsize=25)
-    ax3.set_ylabel("Annual exceedance-rate", fontsize=25)
-    plt.setp(ax3.get_yticklabels(), visible=True, fontsize=25)
-  elif (i==1) and (i+6 == 7):
-    ax3.set_xlabel("Maximum wave amplitude [m]", fontsize=25)
+    ax1.legend(handles=[true_poe, sis_poe, old_if, new_if],#,  bbox_to_anchor=(0, 1.2, 2.05, 0.3), 
+           loc="center right", edgecolor="grey",
+           ncol=1, fontsize=43)
+    ax2.legend(handles=[mc_new_if, new_if],#,  bbox_to_anchor=(0, 1.2, 2.05, 0.3), 
+           loc="center right", edgecolor="grey",
+           ncol=1, fontsize=44)
+    ax3.legend(handles=[true_percentiles1, true_percentiles2, sis_percentiles],#,  bbox_to_anchor=(0, 1.2, 2.05, 0.3), 
+           loc="center right", edgecolor="grey",
+           ncol=1, fontsize=44)
+ 
+    #ax1.set_ylabel("Annual exceedance-rate", fontsize=30)
+    plt.setp(ax1.get_yticklabels(), visible=True, fontsize=55)
+    ax2.set_ylabel("Annual exceedance-rate", fontsize=55)
+    plt.setp(ax2.get_yticklabels(), visible=True, fontsize=55)
+    #ax3.set_ylabel("Annual exceedance-rate", fontsize=30)
+    plt.setp(ax3.get_yticklabels(), visible=True, fontsize=55)
+  #elif (i==1) and (i+6 == 7):
+  #  ax3.set_xlabel("Maximum wave amplitude [m]", fontsize=30)
   else:
     plt.setp(ax1.get_yticklabels(), visible=False)
     plt.setp(ax2.get_yticklabels(), visible=False)
     plt.setp(ax3.get_yticklabels(), visible=False)
   plt.setp(ax1.get_xticklabels(), visible=False)
   plt.setp(ax2.get_xticklabels(), visible=False)
-  plt.setp(ax3.get_xticklabels(), visible=True, fontsize=25)
-  
+  plt.setp(ax3.get_xticklabels(), visible=True, fontsize=55)
+  fig.supxlabel("Maximum wave amplitude [m]", fontsize=55)
  #======================================================================
-
-  axins = inset_axes(ax1, width="50%", height="70%", loc="upper right")
-
+  
+  #axins = inset_axes(ax1, width="52%", height="72%", loc="upper right",
+  #                   bbox_to_anchor=(0.4, 0.43, 0.54, 0.72), bbox_transform=ax1.transAxes)
+  # Set the face color of the inset axes to white and the edge color to black
+  #axins.set_facecolor('white')
+  #axins.spines['top'].set_color('black')
+  #axins.spines['right'].set_color('black')
+  #axins.spines['bottom'].set_color('black')
+  #axins.spines['left'].set_color('black')
+  
   lat_0 = lat.mean()
   lon_0 = lon.mean()
   if inpdir=="CT":
-    m = Basemap(llcrnrlon=15, llcrnrlat=37.3, urcrnrlon=15.17, urcrnrlat=37.550,
+    axins = inset_axes(ax1, width="52%", height="72%", loc="upper right",
+                    bbox_to_anchor=(0.4, 0.43, 0.52, 0.72), bbox_transform=ax1.transAxes)
+    m = Basemap(llcrnrlon=15, llcrnrlat=37.3, urcrnrlon=15.17, urcrnrlat=37.550, #37.3 37.550
             lat_0=lat_0, lon_0=lon_0,
             projection="cyl",
             resolution="f")
     parallels = [37.35, 37.5]
     # labels = [left,right,top,bottom]
-    m.drawparallels(parallels, labels=[1,0,0,0], fontsize=20)
+    m.drawcoastlines(linewidth=2)
+    m.drawparallels([np.round(lat_sis,2)], labels=[1,0,0,0], fontsize=38, color="#000080", textcolor="#000080")#(parallels, labels=[1,0,0,0], fontsize=35)  
     meridians = [15.05, 15.15]
-    m.drawmeridians(meridians, labels=[0,0,0,1], fontsize=20)
+    m.drawmeridians([np.round(lon_sis,2)], labels=[0,0,0,1], fontsize=38, color="#000080", textcolor="#000080")#(meridians, labels=[0,0,0,1], fontsize=35)
+  
   elif inpdir=="SR":
+    axins = inset_axes(ax1, width="55%", height="75%", loc="upper right",
+                     bbox_to_anchor=(0.4, 0.48, 0.55, 0.75), bbox_transform=ax1.transAxes)
     m = Basemap(llcrnrlon=15.2, llcrnrlat=37, urcrnrlon=15.4, urcrnrlat=37.2,
             lat_0=lat_0, lon_0=lon_0,
             projection="cyl",
             resolution="f")
     parallels = [37.05, 37.15]
-    m.drawparallels(parallels, labels=[1,0,0,0], fontsize=20)
+    m.drawcoastlines(linewidth=2)
+    m.drawparallels([np.round(lat_sis,2)], labels=[1,0,0,0], fontsize=38, color="#000080", textcolor="#000080")
     meridians =  [15.25, 15.35] 
-    m.drawmeridians(meridians, labels=[0,0,0,1], fontsize=20)
+    m.drawmeridians([np.round(lon_sis,2)], labels=[0,0,0,1], fontsize=38, color="#000080", textcolor="#000080")
 
-  elif inpdir =="LK":
-    m = Basemap(llcrnrlon=33.55, llcrnrlat=34.85, urcrnrlon=33.75, urcrnrlat=35.05,
-            lat_0=lat_0, lon_0=lon_0,
-            projection="cyl",
-            resolution="f")
-    parallels = [34.9, 35]
-    m.drawparallels(parallels, labels=[1,0,0,0], fontsize=20)
-    meridians = [33.6, 33.7] 
-    m.drawmeridians(meridians, labels=[0,0,0,1], fontsize=20)
-  
   m.drawcoastlines()
   m.drawrivers()
-  m.drawmapboundary(fill_color="#A6CAE0")
+  m.drawmapboundary(fill_color="azure")
 
   
-  m.fillcontinents(color='grey',lake_color='#A6CAE0')
-  m.scatter(lon_sis, lat_sis, marker = 'X', color='purple', s=250, label='SIS POI')
+  m.fillcontinents(color='lightgrey',lake_color='azure')
+  m.scatter(np.round(lon_sis,2), np.round(lat_sis,2), marker = 'X', color='#000080', s=700, label='SIS POI')
   
-  plt.legend(loc="upper center", fontsize=20)
+  plt.legend(loc="upper center", edgecolor='#000080', fontsize=35)
 
 ax1 = axes.flat[0]
-ax1.legend(handles=[true_poe, sis_poe, old_if, new_if, mc_new_if, true_percentiles1, true_percentiles2, sis_percentiles],  bbox_to_anchor=(0, 1.2, 3.1, 0.3), 
-           mode="expand", borderaxespad=0, loc="lower left",
-           ncol=4, fontsize=22)
-
-plt.subplots_adjust(wspace=0.3)
+#ax1.legend(handles=[true_poe, sis_poe, old_if, new_if, mc_new_if, true_percentiles1, true_percentiles2, sis_percentiles],  bbox_to_anchor=(0, 1.2, 2.05, 0.3), 
+#           mode="expand", borderaxespad=0, loc="lower left",
+#           ncol=2, fontsize=38)
+plt.subplots_adjust(wspace=1.0)
 
 fig.tight_layout()
-plt.savefig("VISUALIZATION/offshore_curves_N{}.png".format(percentage))#, dpi=300)
-plt.show()
+#plt.savefig("VISUALIZATION/offshore_curves_N{}.png".format(percentage), dpi=300)
+plt.savefig("VISUALIZATION/offshore_curves_N{}.pdf".format(percentage), format='pdf')
+#plt.show()
 
 
